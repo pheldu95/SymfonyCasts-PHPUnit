0ca2f23801240ae929fa1bf82ee279ba85cbdb7c
pheldu95 <phelps.duncan@gmail.com>
2023-07-12 15:05:28 -0500

    chapter 3. wrote tests to check that we can get and set dino data

--- tests/Unit/Entity/DinosaurTest.php
+++ tests/Unit/Entity/DinosaurTest.php
@@ -2,10 +2,12 @@
 
 namespace App\Tests\Unit\Entity;
 
+use App\Entity\Dinosaur;
 use PHPUnit\Framework\TestCase;
 
 class DinosaurTest extends TestCase
 {
+    /*
     //phpunit will only execute public functions that begin with the word "test". must start with test
     public function testItWorks(): void
     {
@@ -18,5 +20,21 @@ class DinosaurTest extends TestCase
     {
         //assertSame is like doing: if '42' === 42. so this test will fail since the expected is a string
         self::assertSame('42', 42);
+    }
+    */
+
+    public function testCanGetAndSetData(): void
+    {
+        $dino = new Dinosaur(
+            name: 'Big Eaty',
+            genus: 'Tyrannosaurus',
+            length: 15,
+            enclosure: 'Paddock A'
+        );
+
+        self::assertSame('Big Eaty', $dino->getName());
+        self::assertSame('Tyrannosaurus', $dino->getGenus());
+        self::assertSame(15, $dino->getLength());
+        self::assertSame('Paddock A', $dino->getEnclosure());
     }
 }
